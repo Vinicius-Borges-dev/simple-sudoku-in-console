e72212f6010c30240247d577f4d780670087d926
VINICIUS OLIVEIRA BORGES <borges.2970@aluno.pr.senac.br>
2024-08-01 17:09:23 -0300

    Criação de uma função que printa o grid com as casas de escolha

--- index.py
+++ index.py
@@ -8,3 +8,19 @@ def createSudoku():
                 
     return sudoku
 
+def printSudoku(sudoku):
+    linhaExterna = 0
+    colunaExterna = 0
+    print("    ", end="")
+    while linhaExterna <= 8:
+        print(linhaExterna, end="    ")
+        linhaExterna += 1
+    print()
+
+    for linha in sudoku:
+        print(f"{colunaExterna} {linha}")
+        colunaExterna += 1
+        print()
+
+    print()
+    
