9d910f493341d0deea26306e5d051df0162cd5ed
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-08-04 20:53:35 -0300

    Adicionando um sistema de backtracking para inserir números no sudoku até o final

--- index.py
+++ index.py
@@ -3,14 +3,14 @@ import random
 
 
 # Função que valida o sudoku verificando se o número é repetido nas linhas e/ou colunas
-def sudokuValidate(sudoku, linha, coluna, numero):
+def sudokuValidateRowsAndCols(sudoku, row, col, num):
 
-    for i in range(9):
-        if sudoku[linha][i] == numero:
-            return False
-        if sudoku[i][coluna] == numero:
-            return False
+    # percorre todos as colunas da mesma linha e a todas as linhas na mesma coluna
+    for i in range(len(sudoku)):
+        if sudoku[row][i] == num or sudoku[i][col] == num: return False
+        
     return True
+
 
 
 # Função que cria o sudoku percorrendo um range de 0 a 8 criando uma matriz 9x9 preenchida com "_"
@@ -20,41 +20,61 @@ def createSudoku():
     # Cria 9 linhas
     for row in range(9):
         sudoku.append([])
-        # Cria 9 colunas com "_"
+        
+        # Cria 9 colunas e as preenche com "_"
         for col in range(9):
             sudoku[row].append("_")
 
     return sudoku
 
 
-# Função que preenche o sudoku com números aleatórios
+
+
+# Função que preenche o sudoku com números "aleatórios"
 def fillSudoku(sudoku):
 
-    # Cria 30 numeros aleatórios em posições aleatórias
-    for i in range(20):
-
-        # Tenta adicionar um número aleatório com base na validação de linhas e colunas
-        while True:
-            randomRow = random.randint(0, 8)
-            randomCol = random.randint(0, 8)
-            randomNum = str(random.randint(1, 9))
-
-            # Se o espaço estiver vazio e ter validação confirmada, adicionar o número
-            if sudoku[randomRow][randomCol] == "_" and sudokuValidate(
-                sudoku, randomRow, randomCol, randomNum
-            ):
-                sudoku[randomRow][randomCol] = randomNum
-                break
-
-    printSudoku(sudoku)
-
+    # os dois laços foram criados para percorrer todas posições do sudoku
+    for row in range(len(sudoku)):
+        for col in range(len(sudoku)):
+            
+            # se o espaço em questão estiver vazio
+            if sudoku[row][col] == "_":
+                
+                # cria e embaralha os números possiveis
+                nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+                random.shuffle(nums)
+                
+                # para cada número
+                for num in nums:
+                    
+                    # valida se aquele número é repetido na linha ou coluna
+                    if sudokuValidateRowsAndCols(sudoku, row, col, str(num)):
+                        
+                        # Se sim, adiciona aquele número na posição
+                        sudoku[row][col] = str(num)
+                        
+                        # Chama a si próprio para preencher a próxima posição
+                        if fillSudoku(sudoku):
+                            
+                            # se não existir mais espaços restantes, finaliza
+                            return True
+                        
+                        else:
+                            
+                            # Caso o número testado não seja validado naquela posição, deixa em branco para testar outro número
+                            sudoku[row][col] = "_"
+                #  Se nenhum número puder ser inserído, finaliza
+                return False
+            
+    #  Se não tiver mais espaço vazio, termina
+    return True
 
 # Cria uma interface que indica todas as posições posiveis dentro do sudoku
 def printSudoku(sudoku):
     linhaExterna = 0
     colunaExterna = 0
     print("    ", end="")
-    while linhaExterna <= 8:
+    while linhaExterna < len(sudoku):
         print(linhaExterna, end="    ")
         linhaExterna += 1
     print()
@@ -66,6 +86,37 @@ def printSudoku(sudoku):
 
     print()
 
+# função que remove números aleatórios para começar o jogo
+def removeNumbers(sudoku):
+    while True:
+        choice = int(input("Quantos números serão removidos? "))    
+        
+        if choice <= 0 or choice >= len(sudoku) ** 2:
+            print("Escolha um número válido")
+        else:
+            
+            # escolhe randomicamente 5 número a serem tirados do tabuleiro
+            while choice > 0:
+                randomRow = random.randint(0, (len(sudoku) - 1))
+                randomCol = random.randint(0, (len(sudoku) - 1))
+                
+                if sudoku[randomRow][randomCol] != "_":
+                    sudoku[randomRow][randomCol] = "_"
+                    choice -= 1
+            
+            break
+        
+        
+
+def finalVerification(sudoku):
+    count = 0
+    for row in sudoku:
+        count += row.count("_")
+    
+    if count == 0:
+        return True
+    
+    return False
 
 def startGame():
 
@@ -86,23 +137,40 @@ def startGame():
                 # Caso contrário, inicia um loop para pedir o numero a ser adicionado no sudoku
                 while True:
                     choiceNumber = input("Escolha um número entre 1 e 9: ")
-                    if int(choiceNumber) < 1 or int(choiceNumber) > 8:
+                    if int(choiceNumber) < 1 or int(choiceNumber) > 9:
                         print("Número inválido")
                     else:
 
                         # A cada adição, verifica se o numero é repetido na linha ou na coluna em que está inserido
-                        if sudokuValidate(sudoku, choiceRow, choiceCol, choiceNumber):
+                        if sudokuValidateRowsAndCols(sudoku, choiceRow, choiceCol, choiceNumber):
                             sudoku[choiceRow][choiceCol] = choiceNumber
                             printSudoku(sudoku)
+                            if finalVerification(sudoku):
+                                print("Você ganhou!")
+                                return
+                            
                             break
                         else:
                             print("Já existe esse numero na linha ou coluna")
                             continue
+                        
+            
+            
         else:
             print("Campos vazios")
             print()
 
 
+
+
+
+
 sudoku = createSudoku()
+
 fillSudoku(sudoku)
+
+removeNumbers(sudoku)
+
+printSudoku(sudoku)
+
 startGame()
